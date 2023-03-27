64f144c16d8b232c161fe83f4d0d4cef8dd3f6a5
aaron <apuertollanoc@esliceu.net>
2023-03-27 20:44:01 +0200

    examen

--- index.js
+++ index.js
@@ -30,25 +30,45 @@ btnNetejaLlista.onclick = netejaLlista;
 
 //Exercici 1: suma d'hores
 function calculaHora() {
+ 
+    var inputTime1 = document.getElementsByName("inputTime1")[0].value;
+    var inputTime2 = document.getElementsByName("inputTime2")[0].value;
     
+    
+      fecha = new Date(fecha);
+          fecha.setHours(fecha.getHours()+horas);
+          fecha =  fecha.toISOString();
+      var fecha_date = fecha.split('T');
+      var fecha_time = fecha_date[1].split('.');
+      var fecha_time = fecha_date[1].split(':');
+          fecha_date = fecha_date[0];
+  
+      fecha = fecha_date+' '+fecha_time[0]+':'+fecha_time[1];
+    
+    
+      document.getElementsByName("log")[0].value = fecha;
+
 }
 
 function netejaEx1() {
-    
+    document.getElementById("resultatTime").value = "";
 }
 
 // Exercici 2: Arrays
 function generaRandomArray() {
-    
+    let random = Math.random();
+        random = random * 100 + 1;
+        console.log(random);
 }
 
 function netejaEx2() {
-    
+    document.getElementById("resultatArray").value = "";
 }
 
 // Exercici 3: Estils CSS
 function canviaEstil() {
-    
+    document.principal.style.backgroundColor = `rgb(${random()},${random()},${random()})`;
+
 }
 
 function netejaEstil() {
@@ -74,5 +94,5 @@ function pintaDadesJSON(data){
 }
 
 function netejaLlista(){
-   
+    
 }
